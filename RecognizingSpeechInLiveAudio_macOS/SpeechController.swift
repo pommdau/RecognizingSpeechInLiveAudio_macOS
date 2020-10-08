79485024d220862ca9ffe4a7a06613589ac7ab8d
hikeuchi <hikeuchi@morisawa.co.jp>
2020-10-08 16:19:00 +0900

    Speech関連のController Classを作成

---
+++ RecognizingSpeechInLiveAudio_macOS/SpeechController.swift
@@ -0,0 +1,147 @@
+//
+//  SpeechController.swift
+//  RecognizingSpeechInLiveAudio_macOS
+//
+//  Created by HIROKI IKEUCHI on 2020/10/08.
+//  Copyright © 2020 hikeuchi. All rights reserved.
+//
+
+import Foundation
+import Speech
+
+// MARK: - Protocol SpeechControllerDelegate
+
+protocol SpeechControllerDelegate: class {
+    func didReceive(withStatusMessage message: String?, status: SpeechController.RecordingStatus)
+    func didReceive(withTranscription transcription: String, isFilal: Bool)
+}
+
+class SpeechController: NSObject {
+    
+    // MARK: - Properties
+    
+    public enum RecordingStatus: String {
+        case isNotReadyRecording
+        case isReadyRecording
+        case isRecording
+        case isProcessing
+    }
+    
+    private var speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: GeneralPreferences.shared.language))!
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
+    weak var delegate: SpeechControllerDelegate?
+    
+    // MARK: - Lifecycle
+    
+    override init() {
+        super.init()
+        initializeSpeechSettings()
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    public func startRecording() throws {
+        
+        // Cancel the previous task if it's running.
+        recognitionTask?.cancel()
+        self.recognitionTask = nil
+        
+        // Configure the audio session for the app.
+        let inputNode = audioEngine.inputNode
+
+        // Create and configure the speech recognition request.
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        guard let recognitionRequest = recognitionRequest else { fatalError("Unable to create a SFSpeechAudioBufferRecognitionRequest object") }
+        recognitionRequest.shouldReportPartialResults = true
+        
+        // Keep speech recognition data on device
+        if #available(iOS 13, macOS 10.15, *) {
+            recognitionRequest.requiresOnDeviceRecognition = false  // オフライン専用にするならtrue（設定に追加したいような項目）
+        }
+        
+        // Create a recognition task for the speech recognition session.
+        // Keep a reference to the task so that it can be canceled.
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+            var isFinal = false
+            
+            if let result = result {
+                // Update the text view with the results.
+                self.delegate?.didReceive(withTranscription: result.bestTranscription.formattedString, isFilal: false)
+                isFinal = result.isFinal
+            }
+            
+            if error != nil || isFinal {
+                if let result = result {
+                    self.delegate?.didReceive(withTranscription: result.bestTranscription.formattedString, isFilal: true)
+                }
+                
+                // Stop recognizing speech if there is a problem.
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                self.delegate?.didReceive(withStatusMessage: nil, status: .isReadyRecording)
+            }
+        }
+
+        // Configure the microphone input.
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        try audioEngine.start()
+        
+        // Let the user know to start talking.
+        delegate?.didReceive(withStatusMessage: "(Waiting speech..)", status: .isRecording)
+    }
+    
+    private func initializeSpeechSettings() {
+        
+        speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: GeneralPreferences.shared.language))!
+        
+        // Configure the SFSpeechRecognizer object already
+        // stored in a local member variable.
+        speechRecognizer.delegate = self
+        
+        // Asynchronously make the authorization request.
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            
+            // Divert to the app's main thread so that the UI
+            // can be updated.
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                case .authorized:
+                    self.delegate?.didReceive(withStatusMessage: nil, status: .isReadyRecording)
+                case .denied:
+                    self.delegate?.didReceive(withStatusMessage: "User denied access to speech recognition", status: .isNotReadyRecording)
+                case .restricted:
+                    self.delegate?.didReceive(withStatusMessage: "Speech recognition restricted on this device", status: .isNotReadyRecording)
+                case .notDetermined:
+                    self.delegate?.didReceive(withStatusMessage: "Speech recognition not yet authorized", status: .isNotReadyRecording)
+                default:
+                    self.delegate?.didReceive(withStatusMessage: nil, status: .isNotReadyRecording)
+                }
+            }
+        }
+    }
+}
+
+// MARK: - SFSpeechRecognizerDelegate
+
+extension SpeechController: SFSpeechRecognizerDelegate {
+    public func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            delegate?.didReceive(withStatusMessage: nil, status: .isReadyRecording)
+        } else {
+            delegate?.didReceive(withStatusMessage: "recognition Not Available", status: .isNotReadyRecording)
+        }
+    }
+}
